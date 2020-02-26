d55aab369da9ea339ecf2c6d608513b7fa0ae194
doniyor2109 <doniyor2109@gmail.com>
2020-02-26 21:37:28 +0500

    fix: Additional checks prefer-flat

--- rules/prefer-flat.js
+++ rules/prefer-flat.js
@@ -39,22 +39,21 @@ module.exports = {
             'CallExpression[callee.type="MemberExpression"][callee.property.name="reduce"][arguments.length=2][arguments.1.type=ArrayExpression][arguments.1.elements.length=0] > *:function[params.length=2][params.0.type=Identifier][params.1.type=Identifier] > CallExpression[callee.type="MemberExpression"][callee.property.name="concat"][arguments.length=1][arguments.0.type=Identifier]'(node) {
                 const reduceCallbackParams = node.parent.params;
 
-                // concat callee object name must be same as reducer callback accumulator and
-                // concat argument must be same as reducer callback item
-                if(firstElement(node.arguments).name !== secondElement(reduceCallbackParams).name ||
-                    node.callee.object.name !== firstElement(reduceCallbackParams).name
+                // arr.reducer((a, b) => a.concat(b), [])
+                // "concat" function must be called on "a" and concat argument must be "b".
+                if(
+                    firstElement(node.arguments).name === secondElement(reduceCallbackParams).name &&
+                    node.callee.object.name === firstElement(reduceCallbackParams).name
                 ) {
-                    return;
+                    context.report({
+                        node: node.parent.parent,
+                        message: "Use flat to flatten an array",
+                        fix(fixer) {
+                            const sourceCode = context.getSourceCode();
+                            return fixer.replaceText(node.parent.parent, `${sourceCode.getText(node.parent.parent.callee.object)}.flat()`);
+                        }
+                    });
                 }
-
-                context.report({
-                    node: node.parent.parent,
-                    message: "Use flat to flatten an array",
-                    fix(fixer) {
-                        const sourceCode = context.getSourceCode();
-                        return fixer.replaceText(node.parent.parent, `${sourceCode.getText(node.parent.parent.callee.object)}.flat()`);
-                    }
-                });
             }
         };
     }
