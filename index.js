f7d73ae51a1236210239d0fcd8a1c46a6d0dd854
Martin Giger <martin@humanoids.be>
2023-10-10 23:38:56 +0200

    fix: lint3

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  * @author Martin Giger
  */
 import fromMap from "./rules/from-map";
-import noUnnecessaryThisArg from "./rules/no-unnecessary-this-arg";
+import noUnnecessaryThisArgument from "./rules/no-unnecessary-this-arg";
 import preferArrayFrom from "./rules/prefer-array-from";
 import avoidReverse from "./rules/avoid-reverse";
 import preferFlatMap from "./rules/prefer-flat-map";
@@ -12,33 +12,32 @@ import preferFlat from "./rules/prefer-flat";
 const index = {
     rules: {
         "from-map": fromMap,
-        "no-unnecessary-this-arg": noUnnecessaryThisArg,
+        "no-unnecessary-this-arg": noUnnecessaryThisArgument,
         "prefer-array-from": preferArrayFrom,
         "avoid-reverse": avoidReverse,
         "prefer-flat-map": preferFlatMap,
-        "prefer-flat": preferFlat,
+        "prefer-flat": preferFlat
     },
-    configs: {
-        recommended: {
-            plugins: { "array-func": index },
-            rules: {
-                "array-func/from-map": "error",
-                "array-func/no-unnecessary-this-arg": "error",
-                "array-func/prefer-array-from": "error",
-                "array-func/avoid-reverse": "error"
-            }
-        },
-        all: {
-            plugins: { "array-func": index },
-            rules: {
-                "array-func/from-map": "error",
-                "array-func/no-unnecessary-this-arg": "error",
-                "array-func/prefer-array-from": "error",
-                "array-func/avoid-reverse": "error",
-                "array-func/prefer-flat-map": "error",
-                "array-func/prefer-flat": "error"
-            }
-        }
+    configs: {}
+};
+index.configs.recommended = {
+    plugins: { "array-func": index },
+    rules: {
+        "array-func/from-map": "error",
+        "array-func/no-unnecessary-this-arg": "error",
+        "array-func/prefer-array-from": "error",
+        "array-func/avoid-reverse": "error"
+    }
+};
+index.configs.all = {
+    plugins: { "array-func": index },
+    rules: {
+        "array-func/from-map": "error",
+        "array-func/no-unnecessary-this-arg": "error",
+        "array-func/prefer-array-from": "error",
+        "array-func/avoid-reverse": "error",
+        "array-func/prefer-flat-map": "error",
+        "array-func/prefer-flat": "error"
     }
 };
 
